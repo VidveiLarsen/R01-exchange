86eff0e2f0524fd4235083b6328304dbf9f01003
Larsen <vidvei.larsen@gmail.com>
2025-03-15 00:36:10 +0100

    Testing HashMap - replacing command list

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::io::{self, Write};
 use std::error::Error;
 use std::env;
 use exchange::{self, Config, order::BuySell};
+use std::collections::HashMap;
 
 /// Represents all the possible commands that are executable
-pub enum Action
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+enum Action
 {
     Quit, 
     List,
@@ -14,54 +16,57 @@ pub enum Action
     Delete
 }
 
-impl Action
-{
-    pub fn from_string(string: &str) -> Result<Action, Box<dyn Error>>
-    {
-        // todo create static map instead
-        if string == "Q" || string == "Quit"
-        {
-            return Ok(Action::Quit);
-        }
-        else if string == "L" || string == "List"
-        {
-            return Ok(Action::List);
-        }
-        else if string == "H" || string == "Help"
-        {
-            return Ok(Action::Help);
-        }
-        else if string == "B" || string == "Buy"
-        {
-            return Ok(Action::Buy);
-        }
-        else if string == "S" || string == "Sell"
-        {
-            return Ok(Action::Sell);
-        }
-        else if string == "D" || string == "Delete"
-        {
-            return Ok(Action::Delete);
-        }
-        return Err("Cannot convert {string} to Action")?;
-    }
+struct Command {
+    shortcut: String,
+    command: String,
+    description: String
+}
 
-    pub fn list_commands()
-    {
-        println!();
-        println!("Commands(shortcut):");
-        println!("Quit(Q) - Quit");
-        println!("Help(H) - Lists this help text");
-        println!("List(L) - Lists all active orders in the orderbook");
-        println!("Buy(B) - Place buy order");
+impl Command {
+    fn build(shortcut: &str, command: &str, description: &str) -> Command {
+        Command{
+            shortcut: shortcut.to_string(), 
+            command: command.to_string(), 
+            description: description.to_string()
+        }
     }
 }
 
+fn get_commands() -> HashMap<Action, Command> {
+    let mut commands = HashMap::new();
+    commands.insert(Action::Quit, Command::build("Q", "Quit", "Exits the application"));
+    commands.insert(Action::Help, Command::build("H", "Help", "Prints the list of commands"));
+    commands.insert(Action::List, Command::build("L", "List", "Print all orders"));
+    commands.insert(Action::Delete, Command::build("D", "Delete", "Delete order, specified by order_id"));
+    commands.insert(Action::Buy, Command::build("B", "Buy", "Place buy order"));
+    commands.insert(Action::Sell, Command::build("S", "Sell", "Place sale order"));
+    commands
+}
+
+fn list_commands(commands: &HashMap<Action, Command>) {
+    println!("Commands:");
+    commands.iter().for_each(|(_, command)| {
+        println!("{} - {:6}           {:20}", command.shortcut, command.command, command.description);
+    });
+}
+
+fn get_action_from_input(input: &str, commands: &HashMap<Action, Command>) -> Action {
+    commands.iter()
+            .find(|(_, command)|{
+                return input.to_lowercase() == command.command.to_lowercase() || 
+                input.to_lowercase() == command.shortcut.to_lowercase();
+            })
+            .map_or_else(|| {
+                eprintln!("Invalid command!");
+                Action::Help}, 
+                |(action, _)| *action)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let config: Config = Config::build(env::args()).expect("Failed to get config");
     println!("Config: {config:?}");
 
+    let commands = get_commands();
 
     // run input loop
     let mut done: bool = false;
@@ -71,15 +76,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let mut input: String = String::new();
         io::stdin().read_line(&mut input)?;
-        
-        match Action::from_string(input.trim())?
+
+        match get_action_from_input(input.trim(), &commands)
         {
             Action::Quit => {
                 done = true;
                 println!("Quitting");
             },
             Action::Help => {
-                Action::list_commands();
+                list_commands(&commands);
             },
             Action::List => {
                 exchange::list(&config)?;
